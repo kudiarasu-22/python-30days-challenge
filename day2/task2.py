8b5cf07620d988b6bb39f706aba498f6a4fc63f4
kudiarasu-22 <kudiarasushamvel@gmail.com>
2022-12-10 10:09:52 +0530

    add file

--- day2/task2.py
+++ day2/task2.py
@@ -1,10 +1,8 @@
-iimport random
-
+iimport_random
 user_action = input("Enter a choice (rock, paper, scissors): ")
 possible_actions = ["rock", "paper", "scissors"]
 computer_action = random.choice(possible_actions)
 print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
-
 if user_action == computer_action:
     print(f"Both players selected {user_action}. It's a tie!")
 elif user_action == "rock":
